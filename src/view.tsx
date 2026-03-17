9f20ddc5cabce119913115cecf1de2c02b39ef6b
Sam Randa <sr@Mac-299.lan>
2026-03-16 23:06:37 -0400

    Made Vite emit separate index & view routes

--- src/view.tsx
+++ src/view.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Editor from './components/Editor.tsx'
+import Viewer from './components/Viewer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Editor />
+    <Viewer />
   </StrictMode>,
 )
